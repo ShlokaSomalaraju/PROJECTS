f6f843af00362b5edda7249a8958c2d2b923d6d0
ShlokaSomalaraju <132383258+ShlokaSomalaraju@users.noreply.github.com>
2024-09-17 11:34:55 +0530

    Update simple_shell.c

--- Terminal/simple_shell.c
+++ Terminal/simple_shell.c
@@ -11,6 +11,7 @@
     char path[50];
     char prompt[200];
 
+//To fetch the details of the command propt of the terminal window opened
 char* promptfunc(){
     
     getlogin_r(user,sizeof(user));
@@ -26,6 +27,7 @@ char* promptfunc(){
 
 }
 
+// To count the number of Non-space characters
 int countNonspaces(char *str) {
     int count = 0;
     while (*str) {
@@ -39,33 +41,33 @@ int countNonspaces(char *str) {
 
 int main(){
     
-   char *input;
-   int outcome;   
+    char *input;
+    int outcome;   
        
-   while(1){
+    while(1){
  
-   input=readline(promptfunc());
+        input=readline(promptfunc());
 
-    if(countNonspaces(input)==0){
-        continue;
-    }
-    else{
-        add_history(input);
-    } 
+        // To check whether there is a command or else it isn't added to the command history
+        if(countNonspaces(input)==0){
+            continue;
+        }
+        else{
+            add_history(input);
+        } 
    	
-    if(strcmp(input,"exit")==0){
+        if(strcmp(input,"exit")==0){
             break;
-    }
-  
-    else { 
-   	        outcome=system(input);
+        }
+        else { 
+   	        outcome = system(input);
             if(outcome==-1){
             	printf("Invalid command name\n");
             }
-    }
+        }
     
-    free(input);
-   }
+        free(input);
+    }
    
    return 0;
 }
